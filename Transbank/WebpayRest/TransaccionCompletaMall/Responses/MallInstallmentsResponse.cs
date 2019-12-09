d798c8bdb5565876c40151467850ff1de2c77e79
Cesar Mayo <cesar.mayo18@gmail.com>
2019-12-09 16:13:10 -0300

    Create Mall Installments response from Full Mall Transaction

--- Transbank/WebpayRest/TransaccionCompletaMall/Responses/MallInstallmentsResponse.cs
+++ Transbank/WebpayRest/TransaccionCompletaMall/Responses/MallInstallmentsResponse.cs
@@ -1,11 +1,26 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Transbank.Webpay.TransaccionCompletaMall.Common;
-using Transbank.Webpay.TransaccionCompleta.Responses;
-
+    
 namespace Transbank.Webpay.TransaccionCompletaMall.Responses
 {
-    public class MallInstallmentsResponse : InstallmentsResponse { }
+    public class MallInstallmentsResponse
     {
+        [JsonProperty("installments_amount")]
+        public int InstallmentsAmount { get; set; }
+        
+        [JsonProperty("id_query_installments")]
+        public int IdQueryInstallments { get; set; }
+
+        [JsonProperty("deferred_periods")]
+        public List<DeferredPeriods> DeferredPeriods { get; set; }
+
+        public MallInstallmentsResponse(int installmentsAmount, int idQueryInstallments, List<DeferredPeriods> deferredPeriods)
+        {
+            InstallmentsAmount = installmentsAmount;
+            IdQueryInstallments = idQueryInstallments;
+            DeferredPeriods = deferredPeriods;
+        }
+
     }
 }
